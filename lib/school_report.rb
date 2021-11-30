0bd5706cb1ba7c60261fb15f7836fee797e6c11d
francescoFH <francescofacca@gmail.com>
2021-11-30 15:40:43 +0100

    Refactor check_result to return string with result and its total

--- lib/school_report.rb
+++ lib/school_report.rb
@@ -1,7 +1,33 @@
 class SchoolResults
 
   def check_result(string)
-    "Green: 1"
+    results = string.split
+
+    tot_results = Hash.new(0)
+
+    results.each do |v|
+      tot_results[v] += 1
+    end
+
+    tot_results.each do |k, v|
+      return "#{k}: #{v}"
+    end
   end
 
 end
+
+
+# # sample array
+# a=["aa","bb","cc","bb","bb","cc"]
+#
+# # make the hash default to 0 so that += will work correctly
+# b = Hash.new(0)
+#
+# # iterate over the array, counting duplicate entries
+# a.each do |v|
+#   b[v] += 1
+# end
+#
+# b.each do |k, v|
+#   puts "#{k} appears #{v} times"
+# end
--- spec/school_report_spec.rb
+++ spec/school_report_spec.rb
@@ -5,4 +5,7 @@ describe SchoolResults do
   it "returns a string" do
     expect(subject.check_result("Green")).to eq "Green: 1"
   end
+
+  # it "returns a string with result values and total of each values" do
+  #   expect(subject.check_result("Green", "Red", "Green")).to eq "Green: 2\nRed: 1"
 end
